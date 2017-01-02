bd4c87cf8bc819facf6905a5e9d2b8b970ff85e1
samkessaram <samkessaram@gmail.com>
2017-01-02 23:40:23 +0000

    input relevant proj screenshot

--- main.js
+++ main.js
@@ -1,24 +1,45 @@
 $(function(){
-
   $('.project').click(function(){
-    $('#project-view').scrollTop(0)
-    $('body').css('overflow','hidden');
-    $('body').css('height',window.innerHeight * 10);
-    $('#overlay').fadeIn('slow');
-    $('#project-view').css('margin-top', window.scrollY);
-    $('#project-view').css({left:'0%', top: '0px', width: window.innerWidth, height: window.innerHeight});
+    inputProjectInfo(this);
+    showProject();
   })
 
   $('#project-view').click(function(){
-    $('#project-view').css('left','100%');
-    $('#overlay').fadeOut('slow');
-
-    window.setTimeout(function(){
-      $('#project-view').css('width','0px');
-      $('body').css('height',window.innerHeight);
-      $('body').css('overflow','visible');
-    },300);
-
-    
+    hideProject();
   })
 });
+
+function showProject(){
+  $('#project-view').scrollTop(0)
+  $('body').css('overflow','hidden');
+  $('body').css('height',window.innerHeight * 10);
+  $('#overlay').fadeIn('slow');
+  $('#project-view').css('margin-top', window.scrollY);
+  $('#project-view').css({left:'0%', top: '0px', width: window.innerWidth, height: window.innerHeight});
+}
+
+function hideProject(){
+  $('#project-view').css('left','100%');
+  $('#overlay').fadeOut('slow');
+
+  window.setTimeout(function(){
+    $('#project-view').css('width','0px');
+    $('body').css('height',window.innerHeight);
+    $('body').css('overflow','visible');
+  },300); 
+}
+
+function inputProjectInfo(project){
+  $('#screenshot').html($(project).children().clone())
+}
+
+// var projects = {
+//   weather: ,
+//   walkthisway: , 
+//   ohsnap: , 
+//   wikipedia: , 
+//   quotes: , 
+//   todo: 
+// }
+
+// { }
