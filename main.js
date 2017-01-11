345227f289ebecfcd522d48c8d0da8fc35ecee18
samkessaram <samkessaram@gmail.com>
2017-01-11 02:04:16 +0000

    extend padding on job title for iphone overscroll

--- main.js
+++ main.js
@@ -37,11 +37,9 @@ $(function(){
   function fixHeader(){
     if ( document.documentElement.clientWidth <= 720 ){
       if ( window.scrollY > 31 ){
-        $('header, #job-title, #name').addClass('fix');
-        $('#home').addClass('pad');
+        $('header, #job-title, #name, #home').addClass('fix');
       } else {
-        $('header, #job-title, #name').removeClass('fix');
-        $('#home').removeClass('pad');
+        $('header, #job-title, #name, #home').removeClass('fix');
       }
     }
   }
@@ -63,13 +61,12 @@ function setElementDimensions() {
   $('header, h1, #nav').css('max-width',document.documentElement.clientWidth);
   if ( document.documentElement.clientWidth <= 720 ){
     $('#nav').css({ top: window.innerHeight, "margin-top":"-50px"});
-    var padding = Number($('#name').css('height').split('px')[0]) + 50;
-    $('#job-title').css('padding-top', padding);
-    $('#job-title').css('margin-top', '-50px');
+    $('#job-title').css('padding-top', Number($('#name').css('height').split('px')[0]) + 100);
+    $('#job-title').css('margin-top', '-100px')
   } else {
     $('#nav').css({ top: '-30px', "margin-top":"0px"})
     $('#job-title').css('padding-top', '0px')
-    $('#job-title').css('margin-top', '0px');
+    $('#job-title').css('margin-top', '0px')
   }
 }
 
