5ffe58f7b48bf1dad4a28e5c0c9c95977e8b3159
samkessaram <samkessaram@gmail.com>
2017-01-16 21:18:20 +0000

    input project details, formatted spacing of elements

--- main.js
+++ main.js
@@ -16,7 +16,9 @@ $(function(){
   })
 
   $('.project').mouseenter(function(){
-    $('.project').not(this).addClass('filter')
+    if ( document.documentElement.clientWidth > 720 ){
+      $('.project').not(this).addClass('filter')
+    }
   })
 
   $('.project').mouseleave(function(){
@@ -105,6 +107,8 @@ function inputProjectInfo(target){
   $('#screenshot').html($(target).html())
   $('#project-title').html(project.title);
   $('#project-description').html(project.description);
+  $('#project-link').attr('href',project.link);
+  $('#project-source').attr('href',project.source);
 }
 
 
@@ -112,38 +116,39 @@ function inputProjectInfo(target){
 var projects = {
   weather: { 
     title: 'Local Weather',
-    description: 'A fucking weather app duDOI',
-    link: 'https://samkessaram.github.io/local_weather'
+    description: "This is an app I developed while working through the freeCodeCamp curriculum. It simply determines your location and dynamically changes the information, background, and icon according to the conditions fetched from Weather Underground.",
+    link: 'https://samkessaram.github.io/local_weather',
+    source:'link'
   },
-  walkthisway: { 
-    title: 'Walk This Way',
-    description: 'A fucken blurb eh?',
-    link: 'https://blooming-castle-2022.herokuapp.com'
-  }, 
-  ohsnap: { 
-    title: 'Oh Snap!',
-    description: 'A fucken blurb eh?',
-    link: 'https://oh-snap.herokuapp.com/'
+  routefinder: { 
+    title: 'Route Finder',
+    description: "I developed this with some classmates at Lighthouse Labs in Toronto in the fall of 2015. Given the user's location and destination, we would use the Google Maps API and NextBus to determine the most efficient method of transportation. I built the routing algorithm with Ruby on the backend, and helped to style and refine the UI of the app.",
+    link: 'https://blooming-castle-2022.herokuapp.com',
+    source: 'link'
   }, 
   wikipedia: { 
     title: 'Wikipedia Viewer',
-    description: 'A fucken blurb eh?',
-    link: 'https://samkessaram.github.io/wikipedia_viewer'
+    description: "This was an assignment from freeCodeCamp. A user enters a search term, and using the MediaWiki API the app fetches 10 relevant articles. It's a lot prettier than Wikipedia, but I wanted to keep a minimalist aesthetic to keep with the brand.",
+    link: 'https://samkessaram.github.io/wikipedia_viewer',
+    source: 'link'
   }, 
   quotes: { 
     title: 'Random Quote Machine',
-    description: 'A fucken blurb eh?',
-    link: 'https://samkessaram.github.io/random_quote_machine'
+    description: "Another freeCodeCamp project, this fetches quotes from an API, displays them, and allowing the user to tweet them from their account.",
+    link: 'https://samkessaram.github.io/random_quote_machine',
+    source: 'link'
   }, 
   todo: { 
     title: 'Chrome Todo Extension',
-    description: 'A fucken blurb eh?',
-    link: 'https://samkessaram.github.io/chrome-todo'
+    description: "I built this simple&mdash;but useful&mdash;app to learn about Chrome Apps, and ended up learning a lot about Vue and IndexedDB as well. Google is actually phasing out support for Chrome Apps, but this works nicely as a regular web app.",
+    link: 'https://samkessaram.github.io/chrome-todo',
+    source: 'link'
   },
   fiduciary: { 
     title: 'Fiduciary Realty',
-    description: 'A fucken blurb eh?',
-    link: 'https://fiduciaryrealty.com'
+    description: "I was tasked with revamping this real estate agency's website and bringing it up to modern standards. What was once a series of pages full of jargon, with a presentation dating it firmly in the early 1990s, I condensed into a single polished and informative page.",
+    link: 'http://fiduciaryrealty.com',
+    source: 'link'
   }
 }
 
