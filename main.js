ea6c5b4e99d8de2b066501338cbbdbe7fe810540
samkessaram <samkessaram@gmail.com>
2017-01-22 05:35:07 +0000

    added 1px to #nav top (setElementDimensions)

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ $(function(){
 function setElementDimensions() {
   $('header, h1, #nav').css('max-width',document.documentElement.clientWidth);
   if ( document.documentElement.clientWidth <= 720 ){
-    $('#nav').css({ top: window.innerHeight, "margin-top":"-50px"});
+    $('#nav').css({ top: window.innerHeight + 1, "margin-top":"-50px"});
     $('#job-title').css('padding-top', $('#name').css('height'));
   } else {
     $('#nav').css({ top: '-30px', "margin-top":"0px"})
